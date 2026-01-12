470305480afff511e8e3a003525e0b8991f16cfd
TraurigA <andi.traurig@icloud.com>
2026-01-12 19:45:34 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Auto-generated config for your Supabase project
-window.HEIZLOG_CONFIG = {
-  supabaseUrl: "https://lxkgajbdrihoebzchyuc.supabase.co",
-  supabaseKey: "sb_publishable_oxC4Y8rwafneNWLv4QSqtA_nWkC57oR",
-  heatingYearStart: "09-04", // MM-DD (04.09.)
-  defaultTime: "18:00",
-};
